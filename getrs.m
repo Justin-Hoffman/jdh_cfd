7af2c05fae5c8238aee3ef0081b18d545bf4bcb3
Justin <Justin@JustinDeeHoffman.com>
2014-01-22 18:10:48 -0700

    Moved to mgmres.c

--- getrs.m
+++ getrs.m
@@ -1,6 +1,8 @@
-nx = 40
-ny = 40
+nx = 25
+ny = 25
 [xx yy] = meshgrid(linspace(0,1,nx),linspace(0,1,ny));
+[xx1 yy1] = meshgrid(linspace(0,1,nx+1),linspace(0,1,ny+1));
+[xx2 yy2] = meshgrid(linspace(0,1,nx+2),linspace(0,1,ny+2));
 u = frdr('u.dat',nx,ny);
 uraw = frdr('uraw.dat',nx,ny+1);
 us = frdr('us.dat',nx,ny+1);
@@ -10,4 +12,6 @@ vs = frdr('vs.dat',nx+1,ny);
 omega = frdr('omega.dat',nx,ny);
 phi = frdr('phi.dat',nx+1,ny+1);
 strm = frdr('stream.dat',nx+2,ny+2);
+getvo
+figure(3)
 quiver(xx,yy,u',v')
--- getvo.m
+++ getvo.m
@@ -1,25 +1,24 @@
 
-    [nxo nyo] = size(do);
-    [nxv nyv] = size(dv);
+%     [nxo nyo] = size(phi);
+%     [nxv nyv] = size(omega);
     ho = [];
     figure(1)
     close
     figure(2)
     close
     [xx yy] = meshgrid(linspace(0,1,nx),linspace(0,1,ny));
-    
     figure(1)
-    for j=1:2:nyo
-       for i=1:nxo 
-            if(isnan(do(i,j))==0)
-               o(i,1:2) = do(i,j:j+1);
-            end
-       end
-       seenan = 0;
-       hold on
-       plot(o(:,1),o(:,2),'.');
-       clear o
-    end
+%     for j=1:2:nyo
+%        for i=1:nxo 
+%             if(isnan(do(i,j))==0)
+%                o(i,1:2) = do(i,j:j+1);
+%             end
+%        end
+%        seenan = 0;
+%        hold on
+%        plot(o(:,1),o(:,2),'.');
+%        clear o
+%     end
     contour(xx,yy,strm(2:nx+1,2:ny+1)',strmlevels)
     axis equal
     axis([0 1 0 1])
@@ -29,16 +28,16 @@
     
     
     figure(2)
-    for j=1:2:nyv
-       for i=1:nxv 
-            if(isnan(dv(i,j))==0)
-               v(i,1:2) = dv(i,j:j+1);
-            end
-       end
-       hold on
-       plot(v(:,1),v(:,2),'.');
-       clear v
-    end
+%     for j=1:2:nyv
+%        for i=1:nxv 
+%             if(isnan(dv(i,j))==0)
+%                v(i,1:2) = dv(i,j:j+1);
+%             end
+%        end
+%        hold on
+%        plot(v(:,1),v(:,2),'.');
+%        clear v
+%     end
     contour(xx,yy,-omega',omegalevels)
         
     axis equal
