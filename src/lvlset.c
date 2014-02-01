68893f9aa806716450a409793123a113524d031f
Justin <Justin@JustinDeeHoffman.com>
2014-01-31 18:34:44 -0700

    The disk works

--- src/lvlset.c
+++ src/lvlset.c
@@ -4,7 +4,7 @@
  *  Created on: Jan 30, 2014
  *      Author: justin
  */
-#define M_PI 3.14159265358979323846
+#define M_PI 3.14159265358979323846L
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,13 +14,16 @@
 #include "lvlset.h"
 
 double dist_from_arc(double x, double y, double xc, double yc, double R, double t0, double t1){
-	double tc = fmod(atan2((x-xc),(y-yc)),(2.0l* M_PI)); // Angle of current pt
+	double tc = atan2((y-yc),(x-xc)); // Angle of current pt
+	if (tc < 0.0) {
+		tc = 2.0L*M_PI+tc;
+	}
 	if ((tc > t0) && (tc < t1)){ //Point within blanked arc
 		double xp0 = xc + R*cos(t0);
 		double yp0 = yc + R*sin(t0);
 		double xp1 = xc + R*cos(t1);
 		double yp1 = yc + R*sin(t1);
-		return fmax(-dist_from_pt(x,y,xp0,yp0), -dist_from_pt(x,y,xp0,yp0));
+		return -fmin(dist_from_pt(x,y,xp0,yp0), dist_from_pt(x,y,xp1,yp1));
 	} else { //Point outside of blanked arc
 		return dist_from_circ(x,y,xc,yc,R);
 	}
@@ -29,23 +32,34 @@ double dist_from_pt(double x, double y, double xc, double yc){
 	return sqrt((x-xc)*(x-xc) + (y-yc)*(y-yc));
 }
 double dist_from_circ(double x, double y, double xc, double yc, double R){
-	return(-sqrt((x-xc)*(x-xc) + (y-yc)*(y-yc)) + R);
+	return(-dist_from_pt(x,y,xc,yc) + R);
 }
 double dist_from_line(double x, double y, double x0, double y0, double x1, double y1){
-	double t0 = vec_angle((x1-x0),(y1-y0), (x-x0),(y-y0)); // Angle with x0 as vertex
-	double t1 = vec_angle((x0-x1),(y0-y1), (x-x1),(y-y1)); // Angle with x1 as vertex
-	if ( t0 >= 0.0 && t1 >= 0.0){
-		return dist_from_vec((x-x0),(y-x0),(x1-x0),(y1-y0));
+	double xo0 = x0-(y1-y0); //Orthogonal vector
+	double yo0 = y0+(x1-x0); //Orthogonal vector
+	double xo1 = x1-(y1-y0); //
+	double yo1 = y1+(x1-x0); //
+
+	double a0 = vec_angle((x-x0),(y-y0), (xo0-x0),(yo0-y0)); // x -> xo0 with x0 as vertex
+	double a1 = vec_angle((x-x0),(y-y0), (x1-x0),(y1-y0)); // x -> x1 with x0 as vertex
+	double a2 = vec_angle((x-x1),(y-y1), (xo1-x1),(yo1-y1)); // x -> x0 with x1 ax vertex;
+
+	if ((a0>=0.0 && a1 <= 0.0 & a2 <= 0.0) || (a0 >= 0.0 && a1 >= 0.0 && a2 < 0.0) ){
+		return dist_from_vec((x-x0),(y-y0),(x1-x0),(y1-y0));
 	} else {
 		return fmin(dist_from_pt(x,y,x0,y0),dist_from_pt(x,y,x1,y1));
 	}
 }
 double dist_from_vec(double x, double y, double x0, double y0){
-	if (abs(x0) > 0.0 || abs(y0) > 0.0){
-		double sc = vec_dot(x,y,x0,y0)/vec_mag(x0,y0); //vector scale
+	if ((fabs(x0) > 0.0 || fabs(y0) > 0.0)){ //If I won't divide by zero
+		double sc = vec_dot(x,y,x0,y0)/(vec_mag(x0,y0)*vec_mag(x0,y0)); //vector scale
 		return (dist_from_pt(x,y,x0*sc,y0*sc));
 	} else {
-		return vec_mag(x,y);
+		if ((fabs(x0) > 0.0 || fabs(y0) > 0.0)) {
+			return vec_mag(x0,y0);
+		} else {
+			return vec_mag(x,y);
+		}
 	}
 }
 double vec_dot(double x0, double y0, double x1, double y1){
@@ -55,7 +69,13 @@ double vec_cross(double x0, double y0, double x1, double y1){
 	return(x0*y1-x1*y0);
 }
 double vec_angle(double x0, double y0, double x1, double y1){
-	return asin(vec_cross(x0,y0,x1,y1)/(vec_mag(x0,y0)*vec_mag(x1,y1)));
+	double mag1 = vec_mag(x0,y0);
+	double mag2 = vec_mag(x1,y1);
+	if (mag1 != 0 && mag2 != 0){
+		return asin(vec_cross(x0,y0,x1,y1)/(mag1*mag2));
+	} else {
+		return 0.0l;
+	}
 }
 double vec_mag(double x0, double y0){
 	return sqrt(x0*x0+y0*y0);
@@ -80,16 +100,16 @@ double dist_from_box(double x, double y, double x0, double y0, double x1, double
 	}
 }
 double dist_from_notch(double x, double y, double x0, double y0, double x1, double y1, double x2, double y2, double x3, double y3){ //Open sided box
-	/*(-+-+)---(+--+)---(+-+-)-
+	/*(--++)---(+-++)---(+--+)-
 	 *-------------------------
 	 *-------1========2--------
-	 *(--++)-|--------|--------
-	 *-------|-(++++)-|-(++--)-
+	 *(-+++)-|--------|--------
+	 *-------|-(++++)-|-(++-+)-
 	 *-------|--------|-------
 	 *-------|--------|--------
 	 *-------0--------3--------
 	 *-------------------------
-	 *(+-+-)--(-++-)----(-+-+)-
+	 *(-++-)--(+++-)----(++--)-
 	 *-------------------------
 	 */
 	// = or | notch boundary
@@ -106,8 +126,8 @@ double dist_from_notch(double x, double y, double x0, double y0, double x1, doub
 	double d2 = dist_from_line(x,y,x2,y2,x3,y3);
 	double d3 = dist_from_line(x,y,x3,y3,x0,y0);
 	double m0 = fmin(d0,d1);
-	double m1 = fmin(d2,d2);
 
+	/*
 	printf("X%i = %f, Y%i = %f\n",0,x0,0,y0);
 	printf("X%i = %f, Y%i = %f\n",1,x1,1,y1);
 	printf("X%i = %f, Y%i = %f\n",2,x2,2,y2);
@@ -115,31 +135,26 @@ double dist_from_notch(double x, double y, double x0, double y0, double x1, doub
 
 	printf("Angles: %f\t %f\t %f\t %f\t\n",angle0, angle1, angle2, angle3);
 	printf("Distances: %f\t %f\t %f\t %f\t\n",d0, d1, d2, d3);
-	printf("Dist from line check: %f \n\n",dist_from_line(1,1,1,0,2,0));
-
+	*/
 
 	if (angle0 >= 0.0 && angle1 >= 0.0 && angle2 >= 0.0 && angle3 >= 0.0){ // If all cross product angles are greater than 0.0 then point HAS TO BE IN BOX
-		return -fmin(m0,m1);
-	} else if (angle0 >= 0.0 && angle1 <= 0.0 && angle2 >= 0.0 && angle3 <= 0.0){ // Lower left upper right
-		if (d0 < d1 ){ //Lower Left
-			return -d0;
-		} else{ //Upper right
-			return d1;
-		}
-	} else if( angle0 <= 0.0 && angle1 >= 0.0 && angle2 <= 0.0 && angle3 >= 0.0 ) { //Lower right upper left
-		if (d2 < 0){ //Lower right
-			return -d2;
-		} else {
-			return d0;
-		}
-	} else if( angle0 <= 0.0 && angle1 <= 0.0 && angle2 >= 0.0 && angle3 >= 0.0 ) { //Left
+		return -fmin(m0,d2);
+	} else if (angle0 <= 0.0 && angle1 >= 0.0 && angle2 >= 0.0 && angle3 <= 0.0){ // Lower Left
+		return -d0;
+	} else if (angle0 <= 0.0 && angle1 >= 0.0 && angle2 >= 0.0 && angle3 >= 0.0){ // Left
 		return d0;
-	} else if( angle0 >= 0.0 && angle1 <= 0.0 && angle2 <= 0.0 && angle3 >= 0.0 ) { //Top
+	} else if (angle0 <= 0.0 && angle1 <= 0.0 && angle2 >= 0.0 && angle3 >= 0.0){ // Top Left
+		return d0;
+	} else if (angle0 >= 0.0 && angle1 <= 0.0 && angle2 >= 0.0 && angle3 >= 0.0){ // Top
 		return d1;
-	} else if( angle0 >= 0.0 && angle1 >= 0.0 && angle2 <= 0.0 && angle3 <= 0.0 ) { //Right
+	} else if( angle0 >= 0.0 && angle1 <= 0.0 && angle2 <= 0.0 && angle3 >= 0.0 ) { // Top Right
+		return d1;
+	} else if (angle0 >= 0.0 && angle1 >= 0.0 && angle2 <= 0.0 && angle3 >= 0.0){ // Right
 		return d2;
-	} else {
-		return d3;
+	} else if (angle0 >= 0.0 && angle1 >= 0.0 && angle2 <= 0.0 && angle3 <= 0.0){ // Lower Right
+		return -d3;
+	} else { //Lower
+		return -fmin(d0, d3);
 	}
 }
 
@@ -157,9 +172,11 @@ double dist_from_zalesak(double x,double y,double xc,double yc, double R, double
 
 		double dist2notch = dist_from_notch(x, y, xn0, yn0, xn1, yn1, xn2, yn2, xn3, yn3);
 		double dist2arc = dist_from_arc(x,y,xc,yc,R,t0,t1);
-		printf("XC = %f, YC = %f\n", xc,yc);
-		printf("X = %f, Y = %f, d2n = %f, d2a = %f\n", x,y,dist2notch,dist2arc);
-		return fmin(dist2notch,dist2arc);
+		if (dist2notch < 0.0){
+			return dist2notch;
+		} else {
+			return fmin(dist2notch,dist2arc);
+		}
 	} else {//Outside disk
 		return (dist_from_arc(x,y,xc,yc,R,t0,t1));
 	}
@@ -171,10 +188,13 @@ void init_zalesak(double** G, int nx, int ny, int nghost, double dx, double dy){
 	double R = 0.15;
 	double cwidth = 0.05;
 	double cdepth = 0.25;
-	double cangle = M_PI*3.0l/2.0l;
+	double cangle = M_PI*2.5l/2.0l;
 	for (int i = -nghost; i<nx+nghost-1; i++){
 		for (int j = -nghost; j<ny+nghost; j++){
-			G[i+nghost][j+nghost] = dist_from_zalesak((i+nghost)*dx,(j+nghost)*dy,xc,yc,R, cwidth, cdepth, cangle);
+			G[i+nghost][j+nghost] = dist_from_zalesak((i+nghost)*dx+dx/2.0f-nghost*dx,(j+nghost)*dy+dy/2.0f-nghost*dy,xc,yc,R, cwidth, cdepth, cangle);
 		}
 	}
+
+	//printf("\n\n Zalesak Test: D = %f \n\n\n", dist_from_zalesak(0.0, -0.25, 0.0, 0.0, 1.0, .5, 1.5, 3.0L/2.0L*M_PI) );
+
 }
